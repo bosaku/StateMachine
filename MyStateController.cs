e1485629745b9054bca1b35d8d4e5ed06909ef24
bosaku <rbi.noli@gmail.com>
2021-06-24 18:35:41 -0600

    Added a template version of a starting controller
    Start here!

---
+++ MyStateController.cs
@@ -0,0 +1,18 @@
+namespace SFI.GameStates
+{
+    public class MyStateController : StateControllerBase
+    {
+        private void Awake()
+        {
+            Init();
+            
+            //create your states here
+            
+            //add transitions between states here
+            
+            //don't forget to set the starting state!
+            
+            //you should be good to go :)
+        }
+    }
+}
